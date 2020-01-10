c7d5c885628ebfe86f45a940c6e3a11fbb776a30
yalab <rudeboyjet@gmail.com>
2020-01-10 15:11:54 +0900

    dump.sh

--- dump.sh
+++ dump.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-export SOURCE_HOST=$(ruby -ruri -e 'puts URI.parse(ENV["DATABASE_URL"]).host')
-export SOURCE_USER=$(ruby -ruri -e 'puts URI.parse(ENV["DATABASE_URL"]).userinfo' | cut -d ':' -f 1)
-export SOURCE_PORT=$(ruby -ruri -e 'puts URI.parse(ENV["DATABASE_URL"]).port || 3306')
-export SOURCE_PASSWORD=$(ruby -ruri -e 'puts URI.parse(ENV["DATABASE_URL"]).userinfo' | cut -d ':' -f 2)
-export SOURCE_DB=$(ruby -ruri -e 'puts URI.parse(ENV["DATABASE_URL"]).path.delete("/")')
+export HOST=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).host')
+export USER=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).userinfo' | cut -d ':' -f 1)
+export PORT=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).port || 3306')
+export PASSWORD=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).userinfo' | cut -d ':' -f 2)
+export DB=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).path.delete("/")')
 
-mysqldump -u$SOURCE_USER -h$SOURCE_HOST -P$SOURCE_PORT -p$SOURCE_PASSWORD $SOURCE_DB > mysql.dump
-echo $GS_UTIL_KEY_FILE > gs.key
+echo $PRODUCTION_GS_UTIL_KEY_FILE > gs.key
 gcloud auth activate-service-account --key-file=gs.key
-gcloud config set project $GCP_PROJECT_NAME
-gsutil cp mysql.dump gs://$BUCKET_NAME/mysql.dump
+mysqldump -u$USER -h$HOST -P$PORT -p$PASSWORD $DB > mysql.dump
+gsutil cp mysql.dump $PRODUCTION_DUMP_OBJECT_URL
+
