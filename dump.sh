912616223e4cd633485a87962fecd7ecd302e698
yalab <rudeboyjet@gmail.com>
2020-02-28 12:13:33 +0900

    I forget to add `set -eu`.

--- dump.sh
+++ dump.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eu
 export HOST=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).host')
 export USER=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).userinfo' | cut -d ':' -f 1)
 export PORT=$(ruby -ruri -e 'puts URI.parse(ENV["PRODUCTION_DATABASE_URL"]).port || 3306')
@@ -9,4 +10,3 @@ echo $PRODUCTION_GS_UTIL_KEY_FILE > gs.key
 gcloud auth activate-service-account --key-file=gs.key
 mysqldump -u$USER -h$HOST -P$PORT -p$PASSWORD $DB > mysql.dump
 gsutil cp mysql.dump $PRODUCTION_DUMP_OBJECT_URL
-
